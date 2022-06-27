f41d8d5ec47e03b66c5c4ef127098d32febe06f7
Craig Lee <craiglee@fb.com>
2022-06-27 13:39:06 -0700

    pods installed and server conected

---
+++ Instagram/AppDelegate.m
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.m
+//  Instagram
+//
+//  Created by Craig Lee on 6/27/22.
+//
+
+#import "AppDelegate.h"
+#import "Parse/Parse.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(  NSDictionary *)launchOptions {
+
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+        configuration.applicationId = @"VmRkJhX1Mllm6CenayeBFuGCDRerKcWNqhCZ9XIu"; // <- UPDATE
+        configuration.clientKey = @"XIS6U4eqFmjRGaL6gc7KavID14vtlIyZQ8mDfG3l"; // <- UPDATE
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+
+    return YES;
+}
+
+#pragma mark - UISceneSession lifecycle
+
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+
+
+
+@end
